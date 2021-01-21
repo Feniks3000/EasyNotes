3559d01dc8df2e58f0ee475d86b4f0e758e362be
Trofimov <iet@kalite.ro>
2021-01-21 09:52:12 +0300

    Пофиксил отображение recyclerNotes (перекрытие с toolbar) и добавил подстановку имени заметки по-умолчанию

--- app/src/main/java/ru/geekbrains/easynotes/model/Note.kt
+++ app/src/main/java/ru/geekbrains/easynotes/model/Note.kt
@@ -1,5 +1,21 @@
 package ru.geekbrains.easynotes.model
 
 import android.graphics.Color
+import org.apache.commons.lang3.StringUtils
 
-class Note(val title: String = "New note", val body: String, val color: Int = Color.WHITE)
+class Note(title: String, body: String, color: Int = Color.WHITE) {
+    val title: String
+    val body: String
+    val color: Int
+
+    init {
+        if (StringUtils.isNotEmpty(title)) {
+            this.title = title
+        } else {
+            this.title = "New note"
+        }
+        this.body = body
+        this.color = color
+    }
+
+}
