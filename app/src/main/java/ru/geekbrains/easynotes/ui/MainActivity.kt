6a0af031a01e52a3284dc5d7b629dde9714847d6
Trofimov <iet@kalite.ro>
2021-01-21 06:29:21 +0300

    Оформил макеты карточки заметки и MainActivity

--- app/src/main/java/ru/geekbrains/easynotes/ui/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/easynotes/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.geekbrains.easynotes
+package ru.geekbrains.easynotes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.geekbrains.easynotes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
