0cd1f2329fee804d1dc57f9a98d7dc5d2eeb4a22
Trofimov <iet@kalite.ro>
2021-02-11 07:20:04 +0300

    Добавлены тесты для MainViewModel

---
+++ app/src/test/java/ru/geekbrains/easynotes/viewmodel/MainViewModelTest.kt
@@ -0,0 +1,69 @@
+package ru.geekbrains.easynotes.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertFalse
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import ru.geekbrains.easynotes.model.Note
+import ru.geekbrains.easynotes.model.NoteResult
+import ru.geekbrains.easynotes.model.Repository
+
+class MainViewModelTest {
+
+    @get:Rule
+    val taskExecuteRule = InstantTaskExecutorRule()
+
+    private val mockRepository = mockk<Repository>()
+    private val noteLiveData = MutableLiveData<NoteResult>()
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        every { mockRepository.getNotes() } returns noteLiveData
+        viewModel = MainViewModel(mockRepository)
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun `should call getNotes once`() {
+        verify(exactly = 1) { mockRepository.getNotes() }
+    }
+
+    @Test
+    fun `should return error`() {
+        var result: Throwable? = null
+        val testData = Throwable("error")
+
+        viewModel.getViewState().observeForever { result = it?.error }
+        noteLiveData.value = NoteResult.Error(testData)
+
+        assertEquals(result, testData)
+    }
+
+    @Test
+    fun `should return Note`() {
+        var result: List<Note>? = null
+        val testData = listOf(Note(id = "1"), Note(id = "2"))
+
+        viewModel.getViewState().observeForever { result = it?.data }
+        noteLiveData.value = NoteResult.Success(testData)
+
+        assertEquals(result, testData)
+    }
+
+    @Test
+    fun `should remove observer`() {
+        viewModel.onCleared()
+        assertFalse(noteLiveData.hasObservers())
+    }
+}
