d1951cf88e7e2bca81dc13ee916a278da7ad3a37
Trofimov <iet@kalite.ro>
2021-02-16 10:25:03 +0300

    В BaseActivity заменил подписку на ViewState на launch-корутины для слежение за содержимым каналов с данными и ошибками

--- app/src/main/java/ru/geekbrains/easynotes/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/geekbrains/easynotes/ui/base/BaseActivity.kt
@@ -4,24 +4,47 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.consumeEach
 import ru.geekbrains.easynotes.R
 import ru.geekbrains.easynotes.viewmodel.BaseViewModel
+import kotlin.coroutines.CoroutineContext
 
-abstract class BaseActivity<T, VS : BaseViewState<T>> : AppCompatActivity() {
+abstract class BaseActivity<T> : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext by lazy { Dispatchers.Main + Job() }
 
     abstract val ui: ViewBinding
-    abstract val viewModel: BaseViewModel<T, VS>
+    abstract val viewModel: BaseViewModel<T>
+
+    private lateinit var dataJob: Job
+    private lateinit var errorJob: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(ui.root)
+    }
 
-        viewModel.getViewState().observe(this) { t ->
-            t?.apply {
-                data?.let { data -> renderData(data) }
-                error?.let { error -> renderError(error) }
-            }
+    override fun onStart() {
+        dataJob = launch {
+            viewModel.getViewState().consumeEach { renderData(it) }
         }
+
+        errorJob = launch {
+            viewModel.getErrorChannel().consumeEach { renderError(it) }
+        }
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        dataJob.cancel()
+        errorJob.cancel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineContext.cancel()
     }
 
     protected open fun renderError(error: Throwable) = error.message?.let { showError(it) }
